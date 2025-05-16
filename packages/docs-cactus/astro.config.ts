880f6337b8ed5196bca08ba8d96c08f19f327be9
Akashi <akashi_sai@163.com>
2025-05-16 16:50:15 +0800

    :sparkles: update about

--- packages/docs-cactus/astro.config.ts
+++ packages/docs-cactus/astro.config.ts
@@ -10,8 +10,7 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-theme-cactus.netlify.app/",
+	site: "https://aincrad.netlify.app/",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
 		remarkRehype: { footnoteLabelProperties: { className: [""] } },
